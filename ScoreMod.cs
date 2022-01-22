83db22ba86359264fac5c3e96e4115b2fbabecd7
Dominic Aglialoro <domiaglialoro@gmail.com>
2022-01-21 23:03:13 -0800

    Version

--- ScoreMod.cs
+++ ScoreMod.cs
@@ -11,8 +11,8 @@ using UnityEngine;
 namespace SRXDScoreMod; 
 
 // Contains code to initialize the mod
-[BepInDependency("com.pink.spinrhythm.moddingutils", "1.0.1")]
-[BepInPlugin("SRXD.ScoreMod", "ScoreMod", "1.2.0.3")]
+[BepInDependency("com.pink.spinrhythm.moddingutils", "1.0.2")]
+[BepInPlugin("SRXD.ScoreMod", "ScoreMod", "1.2.0.4")]
 public class ScoreMod : BaseUnityPlugin {
     internal new static ManualLogSource Logger { get; private set; }
     internal static ConfigEntry<string> DefaultSystem { get; private set; }
